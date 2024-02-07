971f1a481119396120a71e20f6bbebf72c66eb7c
Wojciech Kowalik <u2062902@unimail.hud.ac.uk>
2024-02-07 19:19:28 +0000

    initial version of counter

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './Counter.css'
+
+export default function Counter() {
+
+
+    const [count, setCount] = useState(0);
+
+    function incrementCounterFunction() {
+        setCount(count + 1)
+        console.log(count)
+        console.log('increment clicked')
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - 1)
+        console.log(count)
+        console.log('decrement clicked')
+    }
+    
+    
+    
+    
+    return (
+        <div className="Counter">
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton" 
+                        onClick={incrementCounterFunction}
+                >+1</button> 
+
+
+
+                <button className="counterButton" 
+                        onClick={decrementCounterFunction}
+                >-1</button>   
+
+            </div>
+
+
+        </div>
+    )
+}
